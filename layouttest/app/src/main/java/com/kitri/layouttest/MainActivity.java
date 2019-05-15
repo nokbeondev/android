bf5655f413374bc23d8e5b313c54fb8bb449f54d
nokbeondev <4789nd@gmail.com>
2019-05-15 15:29:12 +0900

    add LinearLayout test2

--- layouttest/app/src/main/java/com/kitri/layouttest/MainActivity.java
+++ layouttest/app/src/main/java/com/kitri/layouttest/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button btnLinear = findViewById(R.id.btn_linear);
         Button btnRelative = findViewById(R.id.btn_relative);
         Button btnGrid = findViewById(R.id.btn_grid);
         Button btnFrame = findViewById(R.id.btn_frame);
 
         btnLinear.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LinearActivity.class);
@@ -28,28 +29,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnRelative.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, RelativeActivity.class);
+                startActivity(intent);
             }
         });
 
         btnGrid.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, GridActivity.class);
+                startActivity(intent);
             }
         });
 
         btnFrame.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, FrameActivity.class);
+                startActivity(intent);
             }
         });
-
     }
 }
