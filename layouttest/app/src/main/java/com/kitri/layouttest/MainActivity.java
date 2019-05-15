fc52b5b15dcf0c07ff3bb612b952dd1802c96ced
nokbeondev <4789nd@gmail.com>
2019-05-15 13:40:22 +0900

    add LinearLayout test

--- layouttest/app/src/main/java/com/kitri/layouttest/MainActivity.java
+++ layouttest/app/src/main/java/com/kitri/layouttest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kitri.layouttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLinear = findViewById(R.id.btn_linear);
+        Button btnRelative = findViewById(R.id.btn_relative);
+        Button btnGrid = findViewById(R.id.btn_grid);
+        Button btnFrame = findViewById(R.id.btn_frame);
+
+        btnLinear.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRelative.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnGrid.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnFrame.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
